9a713242b8ebc081a17b359b7ba870b6a4c4cfeb
isdabelle <isabelle.pumford@epfl.ch>
2020-04-05 18:59:32 +0200

    Adding all Srolj dependency libraries, working local Solr querying

--- LOAD Network v1.01/src/testing/SolrQuerying.java
+++ LOAD Network v1.01/src/testing/SolrQuerying.java
@@ -1,9 +1,35 @@
 package testing;
-import org.apache.solr.client.solrj
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.request.QueryRequest;
+
 
 public class SolrQuerying {
 	public static void main(String[] args) {
-		public static string fakeURL = "welll"
+		String solrURL = "https://solrdev.dhlab.epfl.ch/solr/impresso_dev";
+		HttpSolrClient client = new HttpSolrClient.Builder(solrURL).build();
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("*:*");
+		solrQuery.set("fl","*");
+		solrQuery.setRows(10);
+		
+		try {
+		    QueryRequest queryRequest = new QueryRequest(solrQuery);
+		    queryRequest.setBasicAuthCredentials("guest_reader","xsBx8NnodUKH");
+		    QueryResponse solrResponse = queryRequest.process(client);
+		    System.out.println(solrResponse);
+		    System.out.println("Total Documents : "+solrResponse.getResults().getNumFound());
+		} catch (SolrServerException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 		
 	}
 }
